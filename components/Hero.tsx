ad52a950245eb30cf185f1a87f5c624e81c37200
dbooker5 <bookerotieno21@gmail.com>
2025-10-23 19:27:03 +0300

    Add Hero component for enhanced portfolio presentation

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import { Spotlight } from './ui/Spotlight'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="pt-36 pb-20">
+        <div>
+          <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+          <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
+          <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+
+        <div className="flex h-screen w-full items-center justify-center absolute top-0 left-0 bg-white dark:bg-grid-white/[0.03] dark:bg-black
+        bg-grid-black/[0.2]">
+      <div className="absolute pointer-events-none  inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black" />
+    </div>
+
+    <div className="flex justify-center relative my-20 z-10">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+            <h1 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                Welcome to My Portfolio
+            </h1>
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Hero
