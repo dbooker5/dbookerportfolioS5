4cc481a911c5eba0368bf5d7589374ab8eed8690
dbooker5 <bookerotieno21@gmail.com>
2025-10-24 21:26:48 +0300

    Introduced BentoGrid Component

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -20,30 +20,52 @@ export const BentoGrid = ({
 };
 
 export const BentoGridItem = ({
-  className,
-  title,
-  description,
-  header,
-  icon,
-  id,
+    className,
+    title,
+    description,
+    id,
+    img,
+    imgClassName,
+    titleClassName,
+    spareImg,
+
 }: {
   className?: string;
   title?: string | React.ReactNode;
   description?: string | React.ReactNode;
   header?: React.ReactNode;
   icon?: React.ReactNode;
-    id: number;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
 }) => {
   return (
     <div
       className={cn(
-        "group/bento shadow-input row-span-1 relative flex flex-col justify-between space-y-4  rounded-3xl border border-red-500 bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
+        "group/bento shadow-input row-span-1 relative flex flex-col justify-between space-y-4  rounded-3xl border bg-white p-4 transition duration-200 hover:shadow-xl dark:border-white/[0.2] dark:bg-black dark:shadow-none",
         className,
       )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+            "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
     >
-      {header}
+        <div className={`${id === 6} && 'flex justify-center h-full'`}>
+            <div className="w-full h-full absolute">
+                {img && (
+                    <img
+                        src={img}
+                        alt={img}
+                        className={cn(imgClassName, 'object-cover, object-center')}
+                    />
+                )}
+            </div>
+        </div>
+
       <div className="transition duration-200 group-hover/bento:translate-x-2">
-        {icon}
         <div className="mt-2 mb-2 font-sans font-bold text-neutral-600 dark:text-neutral-200">
           {title}
         </div>
