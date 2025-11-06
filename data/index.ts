95bb4582b1dcfacd993af46bd243c7b1565e6d3f
dbooker5 <bookerotieno21@gmail.com>
2025-11-06 23:19:09 +0300

    Update project titles and descriptions for clarity and relevance

--- data/index.ts
+++ data/index.ts
@@ -72,15 +72,15 @@ export const gridItems = [
 export const projects = [
   {
     id: 1,
-    title: "3D Solar System Planets to Explore",
-    des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
+    title: "Studio5ive: Crafting a Dynamic Digital Identity",
+    des: "Modern. Creative. Innovative. A digital space built to offer services and inspire ideas.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
     link: "https://github.com/adrianhajdin?tab=repositories",
   },
   {
     id: 2,
-    title: "Yoom - Video Conferencing App",
+    title: "FoodGo - Food Delivery App",
     des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
     img: "/p2.svg",
     iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
