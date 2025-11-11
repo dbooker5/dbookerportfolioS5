ff9353e825bbf61d0fc5547c4801f19dde357ba2
dbooker5 <bookerotieno21@gmail.com>
2025-11-11 23:37:01 +0300

    Add Approach component to Home page for improved content structure

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Approach from "@/components/Approach";
 
 
 export default function Home() {
@@ -20,6 +21,7 @@ export default function Home() {
         <RecentProjects />
         <Clients />
         <Experience />
+        <Approach />
       </div>
     </main>
   );
