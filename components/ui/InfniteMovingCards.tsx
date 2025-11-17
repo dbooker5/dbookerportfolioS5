6532c2c842b2384180675b0346afa215d93fa713
dbooker5 <bookerotieno21@gmail.com>
2025-11-17 18:47:11 +0300

    Refactor InfiniteMovingCards component for improved animation handling and performance

--- components/ui/InfniteMovingCards.tsx
+++ components/ui/InfniteMovingCards.tsx
@@ -106,8 +106,8 @@ export const InfiniteMovingCards = ({
               <div className="relative z-20 mt-6 flex flex-row items-center">
                 <span className="flex flex-col gap-1">
                     <div className="me-3">
-                        <img src="/profile.svg" 
-                        alt="profile" />
+                        <img src="/profile1.svg" 
+                        alt="profile1" />
                     </div>
                     <div className="flex flex-col gap-1">
                         <span className="text-xl leading-[1.6] font-bold  dark:text-white">
